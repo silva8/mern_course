89b0ac215753bf256602c6fff157706f101aa94c
Cristobal Silva <cristobal.isilvap@gmail.com>
2020-06-20 20:50:16 +0800

    express server up and runing

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(PORT, () => console.log('Server up and running'));
+
+
+
+
+
